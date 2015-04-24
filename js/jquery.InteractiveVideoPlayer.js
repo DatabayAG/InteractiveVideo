b129f348571d8d8f3d38aaafdadf1af35ec0ab40
Guido Vollbach <gvollbach@databay.de>
2015-04-24 16:26:43 +0200

    extracted comment list function

--- js/jquery.InteractiveVideoPlayer.js
+++ js/jquery.InteractiveVideoPlayer.js
@@ -57,12 +57,21 @@ $.fn.replaceCommentsAfterSeeking = function (time)
 	{
 		if (comments[i].comment_time <= time && comments[i].comment_text != null && comments[i].is_interactive == 0)
 		{
-			html ='<li> <time class="time">'+ mejs.Utility.secondsToTimeCode(comments[i].comment_time)+ ' </time> ' + comments[i].user_name + ' '  + comments[i].comment_text + '</li>' + html;
+			html = $().buildListElement(comments[i], comments[i].comment_time) + html;
 		}
 	}
 	$("#ul_scroll").html(html);
 };
 
+$.fn.buildListElement = function (comment, time)
+{
+	return '<li>' + 
+				'<time class="time">'+ mejs.Utility.secondsToTimeCode(time)+' </time> ' + 
+		   		'<span class="comment_username">' + comment.user_name + '</span> '  +
+				'<span class="comment_text">' 	  + comment.comment_text + '</span> '  +
+		   '</li>';
+};
+	
 (function ($) {
 
 	il.Util.addOnLoad(function () {
@@ -128,7 +137,7 @@ $.fn.replaceCommentsAfterSeeking = function (time)
 											{
 												if(comments[i].comment_text != null)
 												{
-													$("#ul_scroll").prepend('<li> <time class="time">'+ mejs.Utility.secondsToTimeCode(media.currentTime)+' </time> ' + ' ' + comments[i].user_name + ' '  + comments[i].comment_text + '</li>');
+													$("#ul_scroll").prepend($().buildListElement(comments[i], media.currentTime));
 												}
 												if (comments[i].is_interactive == 1 && $.inArray(comments[i].comment_id, ignore_questions) == -1) 
 												{
