df89ce0c1c5aebf82474744cbb472e608195aa32
Guido Vollbach <gvollbach@databay.de>
2016-02-12 15:19:07 +0100

    fixed ilias version back

--- plugin.php
+++ plugin.php
@@ -8,7 +8,7 @@ $version = '1.0.27';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = '5.0.0';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.1.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible      = 'Nadia Ahmad / Michael Jansen / Max Becker / Guido Vollbach';
