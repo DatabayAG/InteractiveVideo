d4df0df2ef2abf45580d0063998708b842db2e01
Guido Vollbach <gvollbach@databay.de>
2016-02-25 11:31:53 +0100

    removed tests from master branch and increased plugin version

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 $id = 'xvid';
 
 // code version; must be changed for all code changes
-$version = '1.0.29';
+$version = '1.1.0';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
