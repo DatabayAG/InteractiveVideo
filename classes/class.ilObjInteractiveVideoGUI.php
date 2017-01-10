75d33574dffda47d8b30e5ea63a65c9759aa6da4
mjansen <mjansen@databay.de>
2017-01-10 08:39:28 +0100

    Start/End event handling

--- classes/class.ilObjInteractiveVideoGUI.php
+++ classes/class.ilObjInteractiveVideoGUI.php
@@ -193,8 +193,6 @@ class ilObjInteractiveVideoGUI extends ilObjectPluginGUI implements ilDesktopIte
 
 		$video_tpl = new ilTemplate("tpl.video_tpl.html", true, true, $plugin->getDirectory());
 
-		$this->object->trackProgress();
-
 		$object = new ilInteractiveVideoSourceFactoryGUI($this->object);
 		$object->addPlayerElements($tpl);
 
