5970cd32e7e822ea4187a9dffe9c2b9546aae403
Guido Vollbach <gvollbach@databay.de>
2017-03-06 08:25:51 +0100

    minor changes

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+#Partly
+1. Export/Import <= Import not complete yet
+2. Learning Progress <= Preconditions still to do
+
+#Done
+1. Display task above videos
+2. Possibility to create a comment (private/public) on reflection question
+3. Repository object as feedback for a question
+4. Neutral question (without scoring) creation is now possible
+5. CKEditor is now usable for comments and questions
+6. Support for Latex (Mathjax) in question and comments
+7. COPage-Plugin to create links to Interactive Videos
+8. Possibility to create plugins for other video sources
+9. Export of the comment stream
+10. Images for the question text
+11. Extract images for the question text from a file based video source
+12. Youtube as video source
+13. Video platform FAU as media source via plugin
+14. Video platform Vimeo as media source via plugin
+15. Compatibility with PHP 7 and Ilias 5.2.x
--- classes/Form/class.ilInteractiveVideoPreviewPicker.php
+++ classes/Form/class.ilInteractiveVideoPreviewPicker.php
@@ -42,26 +42,40 @@ class ilInteractiveVideoPreviewPicker extends ilImageFileInputGUI
 		$a_tpl->setCurrentBlock("prop_generic");
 		$a_tpl->setVariable("PROP_GENERIC", $link->render());
 		$a_tpl->parseCurrentBlock();
+
+		$this->addModalToTemplate($a_tpl, $ilCtrl);
+
+		$hidden = new ilHiddenInputGUI('extract_file_path');
+		$hidden->setValue($this->getPathToVideo());
+		$hidden->insert($a_tpl);
+	}
+
+	/**
+	 * @param $a_tpl
+	 * @param $ilCtrl
+	 */
+	protected function addModalToTemplate($a_tpl, $ilCtrl)
+	{
 		$modal = ilModalGUI::getInstance();
 		$modal->setId('ffmpeg_extract_modal');
 		$modal->setType(ilModalGUI::TYPE_LARGE);
 		$modal->setHeading(ilInteractiveVideoPlugin::getInstance()->txt('extract'));
 		$video_tpl = new ilTemplate("tpl.ffmpeg_modal.html", false, false, 'Customizing/global/plugins/Services/Repository/RepositoryObject/InteractiveVideo/');
-		$time = new ilInteractiveVideoTimePicker('ffmpeg_time_picker', 'ffmpeg_time_picker');
+
+		$time      = new ilInteractiveVideoTimePicker('ffmpeg_time_picker', 'ffmpeg_time_picker');
 		$video_tpl->setVariable('AJAX_URL', $ilCtrl->getLinkTarget(new ilObjInteractiveVideoGUI(), 'generateThumbnailsFromSourcePerAjax', '', true, false));
 		$video_tpl->setVariable('TIME_PICKER', $time->render());
+
 		$action = ilLinkButton::getInstance();
 		$action->setCaption(ilInteractiveVideoPlugin::getInstance()->txt('extract'), false);
 		$action->setId('generate_ffmpeg');
 		$video_tpl->setVariable('ACTION_BUTTON', $action->render());
 		$video_tpl->setVariable('USE_AS_QUESTION_IMAGE', ilInteractiveVideoPlugin::getInstance()->txt('use_this_image'));
 		$modal->setBody($video_tpl->get());
+
 		$a_tpl->setCurrentBlock("prop_generic");
 		$a_tpl->setVariable("PROP_GENERIC", $modal->getHTML());
 		$a_tpl->parseCurrentBlock();
-		$hidden = new ilHiddenInputGUI('extract_file_path');
-		$hidden->setValue($this->getPathToVideo());
-		$hidden->insert($a_tpl);
 	}
 
 	/**
