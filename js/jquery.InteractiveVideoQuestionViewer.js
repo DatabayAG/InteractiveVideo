754b87cb3f08262de9ba1435bd498de03f4e18e6
Guido Vollbach <gvollbach@databay.de>
2017-04-11 16:11:25 +0200

    fixed link only shows if timing is given

--- js/jquery.InteractiveVideoQuestionViewer.js
+++ js/jquery.InteractiveVideoQuestionViewer.js
@@ -148,11 +148,10 @@ var InteractiveVideoQuestionViewer = (function (scope) {
 				$('#ilQuestionModal').modal('hide');
 				scope.InteractiveVideoPlayerAbstract.jumpToTimeInVideo(feedback.time);
 			});
-
-			if(feedback.feedback_link !== undefined && feedback.feedback_link !== '')
-			{
-				modal.append('<div class="learning_recommendation_link">' + scope.InteractiveVideo.lang.more_information + ': <span class="feedback_link_more">' + '<img src="' + feedback.feedback_icon + '"/>' + feedback.feedback_link + '</span></div>');
-			}
+		}
+		if(feedback.feedback_link !== undefined && feedback.feedback_link !== '')
+		{
+			modal.append('<div class="learning_recommendation_link">' + scope.InteractiveVideo.lang.more_information + ': <span class="feedback_link_more">' + '<img src="' + feedback.feedback_icon + '"/>' + feedback.feedback_link + '</span></div>');
 		}
 	};
 
