38d453fb0d57f3d5021ddcc3730ba0442420933c
Guido Vollbach <gvollbach@databay.de>
2017-11-13 12:30:22 +0100

    moved simple url to video.js

--- VideoSources/core/SimpleUrl/js/jquery.InteractiveVideoSimpleUrlPlayer.js
+++ VideoSources/core/SimpleUrl/js/jquery.InteractiveVideoSimpleUrlPlayer.js
@@ -3,64 +3,46 @@ $( document ).ready(function() {
 });
 
 (function ($) {
+
 	il.Util.addOnLoad(function () {
-		var player = null,
-			seekTime= 0,
-			interval = null;
 		il.InteractiveVideo.last_stopPoint = -1;
-		player = new MediaElementPlayer("#ilInteractiveVideo", {
 
-			timerRate: 50,
-			enablePluginDebug: false,
+		var options = {};
 
-			success: function(media) {
+		var player = videojs('ilInteractiveVideo', options, function onPlayerReady() {
 
-				media.addEventListener('loadeddata', function () {
-					var player = $("video#ilInteractiveVideo")[0];
+			var interval = null;
 
-					il.InteractiveVideoPlayerAbstract.config = {
-						pauseCallback           : (function (){player.pause();}),
-						playCallback            : (function (){player.play();}),
-						durationCallback        : (function (){return player.duration;}),
-						currentTimeCallback     : (function (){return player.currentTime;}),
-						setCurrentTimeCallback  : (function (time){player.setCurrentTime(time);})
-					};
+			il.InteractiveVideoPlayerAbstract.config = {
+				pauseCallback           : (function (){player.pause();}),
+				playCallback            : (function (){player.play();}),
+				durationCallback        : (function (){return player.duration();}),
+				currentTimeCallback     : (function (){return player.currentTime();}),
+				setCurrentTimeCallback  : (function (time){player.setCurrentTime(time);})
+			};
 
-					il.InteractiveVideoPlayerComments.fillEndTimeSelector(il.InteractiveVideoPlayerAbstract.duration());
-				}, false);
+			il.InteractiveVideoPlayerComments.fillEndTimeSelector(il.InteractiveVideoPlayerAbstract.duration());
 
-				media.addEventListener('loadedmetadata', function () {
-					if (seekTime > 0) {
-						media.currentTime = seekTime;
-						seekTime = 0;
-					}
-				}, false);
+			this.on('seeked', function() {
+				clearInterval(interval);
+				il.InteractiveVideoPlayerFunction.seekingEventHandler();
+			});
 
-				media.addEventListener('play', function() {
-					il.InteractiveVideoPlayerAbstract.play();
-				}, false);
+			this.on('pause', function() {
+				clearInterval(interval);
+				il.InteractiveVideo.last_time = il.InteractiveVideoPlayerAbstract.currentTime();
+			});
 
-				media.addEventListener('seeked', function() {
-					clearInterval(interval);
-					il.InteractiveVideoPlayerFunction.seekingEventHandler();
-				}, false);
+			this.on('ended', function() {
+				il.InteractiveVideoPlayerAbstract.videoFinished();
+			});
 
-				media.addEventListener('pause', function() {
-					clearInterval(interval);
-					il.InteractiveVideo.last_time = il.InteractiveVideoPlayerAbstract.currentTime();
-				}, false);
+			this.on('playing', function() {
+				interval = setInterval(function () {
+					il.InteractiveVideoPlayerFunction.playingEventHandler(interval, player);
+				}, 500);
 
-				media.addEventListener('ended', function() {
-					il.InteractiveVideoPlayerAbstract.videoFinished();
-				}, false);
-
-				media.addEventListener('playing', function() {
-					interval = setInterval(function () {
-						il.InteractiveVideoPlayerFunction.playingEventHandler(interval, player);
-					}, 500);
-
-				}, false);
-			}
+			});
 		});
 	});
 })(jQuery);
