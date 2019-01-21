f1fdb3232e18e7a75e56165f88a3020fd44de022
Guido Vollbach <gvollbach@databay.de>
2019-01-21 14:51:54 +0100

    added cleanup routine

--- js/InteractiveVideoPlayerResume.js
+++ js/InteractiveVideoPlayerResume.js
@@ -8,8 +8,7 @@ il.InteractiveVideoPlayerResume = (function (scope) {
 
 	pub.checkForResumeValue = function (player_id) {
 		setTimeout(function () {
-			let key = pro.getStorageKey(player_id);
-
+			let key = pri.getStorageKey(player_id);
 			if (pro.IsStorageAvailable()) {
 				let saved_time = parseFloat(pri.storage_media.getItem(key));
 				let duration = scope.InteractiveVideoPlayerAbstract.duration(player_id);
@@ -18,37 +17,75 @@ il.InteractiveVideoPlayerResume = (function (scope) {
 					saved_time = 0.0001;
 				}
 
-				scope.InteractiveVideoPlayerAbstract.setCurrentTime(player_id, saved_time);
-				pro.removeExistingKey();
+				scope.InteractiveVideoPlayerAbstract.setCurrentTime(saved_time, player_id);
+				pro.removeExistingKey(player_id);
 			}
 		}, 250);
 	};
 
 	pub.saveResumeTime = function (player_id) {
-		let key = pro.getStorageKey(player_id);
+		let key = pri.getStorageKey(player_id);
 		let current_time = scope.InteractiveVideoPlayerAbstract.currentTime(player_id);
 
 		if (pro.IsStorageAvailable()) {
 			if (current_time > 0) {
-				pri.storage_media.setItem(key, current_time);
+				pri.writeItemToStorage(key, current_time, player_id);
 			}
 		}
 	};
 
 	pro.removeExistingKey = function (player_id) {
-		let key = pro.getStorageKey(player_id);
-
+		let key = pri.getStorageKey(player_id);
 		if (pro.IsStorageAvailable()) {
 			pri.storage_media.removeItem(key);
 		}
 	};
 
-	pro.getStorageKey = function (player_id) {
+	pri.getStorageKey = function (player_id) {
 		let player_config = scope.InteractiveVideo[player_id];
-		let ref_id = parseInt(player_config.interactive_video_ref_id, 10);
-		let client_id = player_config.installation_client_id;
+		let ref_id        = pri.getRefIdFromPlayerConfig(player_config);
+		let client_id     = player_config.installation_client_id;
 
 		return pri.storage_key + '_' + client_id + '_' + ref_id;
+	};
+
+	pri.getRefIdFromPlayerConfig = function(player_config) {
+
+		return parseInt(player_config.interactive_video_ref_id, 10);
+	};
+
+	pri.writeItemToStorage = function(key, current_time, player_id)  {
+		let ref_id         = pri.getRefIdFromPlayerConfig(scope.InteractiveVideo[player_id]);
+		let data_grave     = pri.getDataGraveObject();
+		let data_grave_key = pri.storage_key + '_DataGrave';
+		let d = new Date();
+		let n = d.getTime();
+
+		data_grave[ref_id] = n;
+		pri.storage_media.setItem(key, current_time);
+		pri.storage_media.setItem(data_grave_key, JSON.stringify(data_grave));
+	};
+
+	pub.cleanUpStorage = function(player_id) {
+		let data_grave     = pri.getDataGraveObject();
+		let player_config  = scope.InteractiveVideo[player_id];
+		let client_id      = player_config.installation_client_id;
+
+		$.each(data_grave, function( ref_id, timestamp ) {
+			console.log(  pri.storage_key + '_' + client_id + '_' + ref_id + ": " + timestamp );
+			// pri.storage_media.removeItem(pri.storage_key + '_' + client_id + '_' + ref_id );
+		});
+	};
+	
+	pri.getDataGraveObject = function() {
+		let data_grave_key = pri.storage_key + '_DataGrave';
+		let data_grave     = JSON.parse(pri.storage_media.getItem(data_grave_key));
+
+		if(data_grave == undefined ) {
+			data_grave = {};
+		}
+
+		return data_grave
 	};
 
 	pro.IsStorageAvailable = function () {
