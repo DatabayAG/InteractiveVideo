c4602fbd29857729fa4363ac9e6adb389172bafc
Guido Vollbach <gvollbach@databay.de>
2019-03-15 09:51:09 +0100

    cleanup

--- js/InteractiveVideoSubtitle.js
+++ js/InteractiveVideoSubtitle.js
@@ -2,18 +2,19 @@ il.InteractiveVideoSubtitle = (function (scope) {
 	'use strict';
 
 	let pub = {}, pro = {}, pri = {
-
+		subtitles : 'subtitles'
 	};
 
 	pub.initializeSubtitleTracks = function(player_id){
 		$.each(il.InteractiveVideo[player_id].tracks[0], function (key, value) {
-			var track = document.createElement('track');
-			track.kind = 'subtitles';
-			track.label = value.label;
-			track.src = value.src;
-			track.srclang = value.srclang;
-			$("#" + player_id).append(track);
 
+			let track = document.createElement('track');
+			track.kind    = pri.subtitles;
+			track.label   = value.label;
+			track.src     = value.src;
+			track.srclang = value.srclang;
+
+			$("#" + player_id).append(track);
 
 		});
 	};
