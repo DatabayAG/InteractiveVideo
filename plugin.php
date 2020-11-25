f44d6251b8868972558f0bec5fa59508030d5b8c
Guido Vollbach <gvollbach@databay.de>
2020-11-25 06:29:07 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@ $version = '2.0.26';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = '5.1.0';
+$ilias_min_version = '5.2.0';
 $ilias_max_version = '5.4.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
