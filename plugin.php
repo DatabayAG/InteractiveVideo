f720b5f4f65f9e1307d4378659267dbff82dbec7
Guido Vollbach <gvollbach@databay.de>
2023-06-02 11:39:46 +0200

    Fix plugin version

--- plugin.php
+++ plugin.php
@@ -3,12 +3,12 @@
 $id = 'xvid';
 
 // code version; must be changed for all code changes
-$version = "3.1.27";
+$version = "3.5.00";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = '6.0.0';
-$ilias_max_version = '7.99';
+$ilias_min_version = '8.0';
+$ilias_max_version = '8.99';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible      = 'Nadia Ahmad / Michael Jansen / Guido Vollbach';
