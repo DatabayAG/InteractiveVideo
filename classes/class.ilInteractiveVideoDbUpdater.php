99a48a44c42207910fb209a0e3c847f392251a89
Guido Vollbach <gvollbach@databay.de>
2023-09-29 09:45:08 +0200

    Fixed db updates after patch

--- classes/class.ilInteractiveVideoDbUpdater.php
+++ classes/class.ilInteractiveVideoDbUpdater.php
@@ -154,7 +154,7 @@ class ilInteractiveVideoDbUpdater extends ilPluginDBUpdate
 			$this->readLastUpdateFile();
 			$version = $this->readFileVersion();
 			$return_value = $this->applyUpdate();
-			if($return_value)
+            if(is_null($return_value) || $return_value === true)
 			{
 				$this->setCurrentVersion($version);
 			}
