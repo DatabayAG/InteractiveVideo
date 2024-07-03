77d1d0e8a422aba20bd1bba0e36100ffb43094ec
Guido Vollbach <gvollbach@databay.de>
2024-07-03 08:53:13 +0200

    Changed layout of import and config

--- js/form/InteractiveVideoCKEditorInit.js
+++ js/form/InteractiveVideoCKEditorInit.js
@@ -1,56 +1,39 @@
 import {
     ClassicEditor,
-    AccessibilityHelp,
-    AutoLink,
-    Autosave,
-    Bold,
-    Essentials,
-    Italic,
-    Link,
-    Paragraph,
-    SelectAll,
-    SpecialCharacters,
-    Strikethrough,
-    Subscript,
-    Superscript,
-    Underline,
-    Undo
+    AccessibilityHelp, AutoLink, Autosave, Bold, Essentials, Italic, Link, Paragraph, SelectAll,
+    SpecialCharacters, Strikethrough, Subscript, Superscript, Underline, Undo
 } from '../../libs/npm/node_modules/ckeditor5/dist/browser/ckeditor5.js';
-
-
 
 il.InteractiveVideoEditor = (function (scope) {
     'use strict';
 
     let pub = {}, pro = {},
         pri = {
-            editor_instances : {}
-    };
+            editor_instances: {}
+        };
 
-    pub.createMultipleInstances = function(elementIdentifiers) {
+    pub.createMultipleInstances = function (elementIdentifiers) {
         elementIdentifiers.forEach((elementIdentifier) => pub.createInstance(elementIdentifier));
     }
 
-    pub.createInstance = function(elementIdentifier) {
-        if( ! pub.getEditorInstanceById(elementIdentifier)) {
-            if(document.getElementById(elementIdentifier)) {
+    pub.createInstance = function (elementIdentifier) {
+        if ( ! pub.getEditorInstanceById(elementIdentifier)) {
+            if (document.getElementById(elementIdentifier)) {
                 return ClassicEditor
-                    .create( document.querySelector( '#' + elementIdentifier ), pri.getEditorConfig())
-                    .then( editor => {
-                        pri.editor_instances[ elementIdentifier ] = editor;
-                        editor.editing.view.change( writer => {
+                    .create(document.querySelector('#' + elementIdentifier), pri.getEditorConfig())
+                    .then(editor => {
+                        pri.editor_instances[elementIdentifier] = editor;
+                        editor.editing.view.change(writer => {
                             writer.setStyle('min-height', '75px', editor.editing.view.document.getRoot());
-                        } );
-                    } )
-                    .catch( err => console.error( err.stack ) );
+                        });
+                    })
+                    .catch(err => console.error(err.stack));
             }
         }
-
     }
 
-    pub.getEditorInstanceById = function (elementIdentifier)
-    {
-        if(pub.getEditorInstancesCount() >= 1) {
+    pub.getEditorInstanceById = function (elementIdentifier) {
+        if (pub.getEditorInstancesCount() >= 1) {
             if (typeof pri.editor_instances[elementIdentifier] != "undefined") {
                 return pri.editor_instances[elementIdentifier];
             }
@@ -63,48 +46,22 @@ il.InteractiveVideoEditor = (function (scope) {
         return Object.keys(pri.editor_instances).length;
     }
 
-    pub.getEditorInstances = function ()
-    {
+    pub.getEditorInstances = function () {
         return pri.editor_instances;
     }
 
-    pri.getEditorConfig = function()
-    {
+    pri.getEditorConfig = function () {
         return {
             toolbar: {
                 items: [
-                    'undo',
-                    'redo',
-                    '|',
-                    'bold',
-                    'italic',
-                    'underline',
-                    'strikethrough',
-                    'subscript',
-                    'superscript',
-                    '|',
-                    'link',
-                    '|',
-                    'accessibilityHelp'
+                    'undo', 'redo', '|', 'bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript',
+                    '|', 'link', '|', 'accessibilityHelp'
                 ],
                 shouldNotGroupWhenFull: false
             },
             plugins: [
-                AccessibilityHelp,
-                AutoLink,
-                Autosave,
-                Bold,
-                Essentials,
-                Italic,
-                Link,
-                Paragraph,
-                SelectAll,
-                SpecialCharacters,
-                Strikethrough,
-                Subscript,
-                Superscript,
-                Underline,
-                Undo
+                AccessibilityHelp, AutoLink, Autosave, Bold, Essentials, Italic, Link, Paragraph, SelectAll,
+                SpecialCharacters, Strikethrough, Subscript, Superscript, Underline, Undo
             ],
             link: {
                 addTargetToExternalLinks: true,
