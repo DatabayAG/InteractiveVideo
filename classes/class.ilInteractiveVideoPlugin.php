cf6887cc46fca7018adb72b20cd4cc71a489e91b
Guido Vollbach <gvollbach@databay.de>
2025-04-24 06:12:30 +0200

    Fix #44868

--- classes/class.ilInteractiveVideoPlugin.php
+++ classes/class.ilInteractiveVideoPlugin.php
@@ -116,6 +116,6 @@ class ilInteractiveVideoPlugin extends ilRepositoryObjectPlugin
         if(is_null($a_str)) {
             return '';
         }
-        return ilUtil::stripSlashes($a_str, true, '<a><br><strong><em><span><p><sub><sup>');
+        return ilUtil::stripSlashes($a_str, true, '<a><br><i><u><s><strong><em><span><p><sub><sup>');
     }
 }
