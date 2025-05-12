a9bb6f06c573745647e55f81ca8944a4ec82cb2d
Guido Vollbach <gvollbach@databay.de>
2025-05-12 07:07:21 +0200

    R9 dev (#81)
    * Fix #44868
    
    * Fix #45042
    
    * Fix #44915
    
    * Fix mathjax support for version 3, thx Mirco!

--- js/InteractiveVideoPlayerFunctions.js
+++ js/InteractiveVideoPlayerFunctions.js
@@ -607,9 +607,31 @@ il.InteractiveVideoPlayerFunction = (function (scope) {
         });
     };
 
-    pub.refreshMathJaxView = function () {
+    pub.refreshMathJaxView = function (elements, reprocess = false) {
         if (typeof MathJax !== 'undefined') {
-            MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+            if (typeof MathJax.Hub !== 'undefined') {
+                // MathJax 2
+                if (reprocess) {
+                    MathJax.Hub.Queue(['Reprocess', MathJax.Hub, elements]);
+                } else {
+                    MathJax.Hub.Queue(['Typeset', MathJax.Hub, elements]);
+                }
+            } else {
+                // MathJax 3
+                const interval_id = setInterval((resolve, reject) => {
+                    if (typeof MathJax.startup.promise !== 'undefined') {
+                        clearInterval(interval_id);
+                        MathJax.startup.promise = MathJax.startup.promise
+                          .then(() => {
+                              if (reprocess) {
+                                  MathJax.typesetClear(elements);
+                              }
+                              MathJax.typesetPromise()
+                                .catch((err) => console.log(`MathJax typesetting failed: ${err.message}`));
+                          });
+                    }
+                });
+            }
         }
     };
 
