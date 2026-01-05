6bc1998daa69cd796eef43147d672391253723eb
Guido Vollbach <gvollbach@databay.de>
2026-01-05 08:04:45 +0100

    Stash

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -1362,5 +1362,33 @@ if(!$ilDB->tableExists('rep_robj_xvid_youtube'))
     $ilDB->addPrimaryKey('rep_robj_xvid_youtube', array('obj_id', 'youtube_id'));
 }
 ?>
+<#88>
+<?php
+/**
+ * @var $ilDB ilDB
+ */
+if (!$ilDB->tableExists('rep_robj_xvid_opc')) {
+    $fields = array(
+            'obj_id' => array(
+                    'type' => 'integer',
+                    'length' => '4',
+                    'notnull' => true
+            ),
+            'opc_id' => array(
+                    'type' => 'text',
+                    'length' => '100',
+                    'notnull' => true
+            ),
+            'opc_url' => array(
+                    'type' => 'text',
+                    'length' => '1000',
+                    'notnull' => true
+            )
+    );
+    $ilDB->createTable('rep_robj_xvid_opc', $fields);
+    $ilDB->addPrimaryKey('rep_robj_xvid_opc', array('obj_id', 'opc_id'));
+}
+?>
+
 
 
