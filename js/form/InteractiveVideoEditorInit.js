f9fb72fe7c0f325bcdd10fd9767069c5fd9c5bb9
Guido Vollbach <gvollbach@databay.de>
2026-04-07 12:41:02 +0200

    Fix reinit for text fields

--- js/form/InteractiveVideoEditorInit.js
+++ js/form/InteractiveVideoEditorInit.js
@@ -17,8 +17,7 @@ il.InteractiveVideoEditor = (function (scope) {
     }
 
     pub.createInstance = function (elementIdentifier) {
-      //Todo: check if this is really needed
-        if (! pub.getEditorInstanceById(elementIdentifier)) {
+        //if (! pub.getEditorInstanceById(elementIdentifier)) {
             if (document.getElementById(elementIdentifier)) {
                 return ClassicEditor
                     .create(document.querySelector('#' + elementIdentifier), pri.getEditorConfig())
@@ -30,7 +29,7 @@ il.InteractiveVideoEditor = (function (scope) {
                     })
                     .catch(err => console.error(err.stack));
             }
-        }
+       // }
     }
 
     pub.getEditorInstanceById = function (elementIdentifier) {
